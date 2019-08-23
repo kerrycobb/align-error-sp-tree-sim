e80038f7d7405139eee1647b7004d7996580dc0a
Kerry Cobb <cobbkerry@gmail.com>
2019-08-23 10:05:10 -0500

    added error sim to simulation

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import fire
+import glob
 import yaml
 import fire
 import subprocess
@@ -9,8 +10,14 @@ import jinja2 as jj
 import dendropy as dp
 import math
 import random
+import numpy as np
+from Bio import AlignIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
+from Bio.Alphabet import generic_dna
 
-def gen_out_dirs(seed, config):
+def gen_out_dirs(seed, config, ignore_existing=False):
     contain_dir = os.path.abspath(
         "out-sp{sp}-gen{gen}-loc{loc}-len{len}".format(
             sp=config["nspecies"],
@@ -22,7 +29,11 @@ def gen_out_dirs(seed, config):
     out_dir = os.path.join(
         contain_dir, 
         "seed{}-reps{}".format(seed, config["nreps"]))
-    os.mkdir(out_dir)
+    try:
+        os.mkdir(out_dir)
+    except:
+        if ignore_existing:
+            pass
     return out_dir
 
 def sim_species_tree(rng, config):
@@ -60,8 +71,10 @@ def sim_gene_trees(sp_tree, rng, config):
     return gene_trees
 
 def gen_alignments(dir, rng, config, gene_trees):
+    out_dir = os.path.join(dir, "singleton-prob-1.0")
+    os.mkdir(out_dir)
     for ix, tree in enumerate(gene_trees):
-        alignment_path = os.path.join(dir, "alignment-{}.phy".format(ix))
+        out_path = os.path.join(out_dir, "alignment-{}.phy".format(ix))
         newick_string = tree.as_string("newick", suppress_rooting=True)
         seq_gen_script = [
             "seq-gen",
@@ -73,8 +86,57 @@ def gen_alignments(dir, rng, config, gene_trees):
             "-op", 
             "-q",
             "<<<", "\"{}\"".format(newick_string),
-            ">", alignment_path]
+            ">", out_path]
         subprocess.call(" ".join(seq_gen_script), shell=True)
+
+def gen_error(dir, rng, config):
+    for prob in config["singleton_sample_prob"]:
+        out_dir = os.path.join(dir, "singleton-prob-{}".format(prob))
+        os.mkdir(out_dir)
+        singletons = 0
+        rmvd_singletons = 0       
+        for locus in range(0, config["nloci"]):
+            align_path = os.path.join(
+                dir, 
+                "singleton-prob-1.0", 
+                "alignment-{}.phy".format(locus))
+            dna = AlignIO.read(open(align_path), "phylip")
+            ids = []
+            seqs = [] 
+            for record in dna:
+               ids.append(record.id)
+               seqs.append(np.array(list(record.seq))) 
+            matrix = np.stack(seqs)
+            print(matrix)
+            for column in range(0, matrix.shape[1]):
+                g = np.count_nonzero(matrix[...,column] == "G")
+                t = np.count_nonzero(matrix[...,column] == "T")
+                if g == 1:
+                    singletons += 1
+                    if rng.uniform(0, 1) >= prob: 
+                        rmvd_singletons += 1
+                        for i in range(0, matrix.shape[0]):
+                            matrix[i, column] = "T"                      
+                elif t == 1:
+                    singletons += 1
+                    if rng.uniform(0, 1) >= prob:
+                        rmvd_singletons += 1
+                        for i in range(0, matrix.shape[0]):
+                            matrix[i, column] = "G"
+            print(matrix)
+            new_seqs = ["".join(row) for row in matrix]
+            new_seq_records = [] 
+            for id, new_seq in zip(ids, new_seqs):
+                new_seq_records.append(SeqRecord(Seq(new_seq), id=id))
+            new_align = MultipleSeqAlignment(new_seq_records)
+            AlignIO.write(
+                new_align, 
+                os.path.join(out_dir, "alignment-{}.phy".format(locus)), 
+                "phylip") 
+        # TODO: Store data that could be used to plot histogram of singletons per locus
+        stats = dict(singletons=singletons, removed_singletons=rmvd_singletons)
+        with open(os.path.join(out_dir, "stats.yaml"), "w") as fh:
+            yaml.dump(stats, fh)
 
 def gen_xml(dir, config):
     taxa = ["T{}".format(i) for i in range(1, config["nspecies"]+1)] 
@@ -128,49 +190,60 @@ def gen_ecoevol_alignment(dir, config, gene_trees):
         d[key] = "".join(value)
     matrix = dp.StandardCharacterMatrix.from_dict(d)
     matrix.write(path=os.path.join(dir, "alignment.nex"), schema="nexus")
-    
-def gen_data(seed, config_path):
-    subprocess.call("module load seq-gen; module load simphy", shell=True)
+
+def gen_data(seed, config_path, eco_path):
     config = yaml.safe_load(open(config_path))
-
-    # Create output directories
-    out_dir = gen_out_dirs(seed, config)
-
-    # Copy configs into output directory
-    new_config_path = os.path.join(out_dir, "config.yml")
-    yaml.dump(config, open(new_config_path, "w"))
-    # new_eco_config_path = os.path.join(out_dir, "eco-config.yml")
-    # yaml.dump(eco_config, open(new_eco_config_path, "w"))
+    eco_config = yaml.safe_load(open(eco_path))
 
     rng = random.Random(seed)
+
+    # Create output directories
+    out_dir = gen_out_dirs(seed, config, ignore_existing=True)
+
+    # Copy configs into output directory
+    config["seed"] = rng.randint(0, 1000000000)
+    new_config_path = os.path.join(out_dir, "config.yml")
+    yaml.dump(config, open(new_config_path, "w"))
+    new_eco_config_path = os.path.join(out_dir, "eco-config.yml")
+    yaml.dump(eco_config, open(new_eco_config_path, "w"))
 
     # Simulate data
     for rep in range(0, config["nreps"]):
         rep_dir = os.path.join(out_dir, "rep-{}".format(rep))
         os.mkdir(rep_dir)
+
         # Simulate trees
         sp_tree = sim_species_tree(rng=rng, config=config)
         sp_tree.write(
             path=os.path.join(rep_dir, "sp-tree.tre"), 
             schema="newick", 
             suppress_rooting=True)
+
         # Generate gene trees
         gene_trees = sim_gene_trees(sp_tree=sp_tree, rng=rng, config=config)
         gene_trees.write(
             path=os.path.join(rep_dir, "gene-trees.tre"), 
             schema="newick", 
             suppress_rooting=True)
-        # Generate gene trees    
+
+        # Generate alignments    
         gen_alignments(
             dir=rep_dir, 
             rng=rng,
             config=config,
             gene_trees=gene_trees)
 
-        # Generate xml 
-        gen_xml(dir=rep_dir, config=config)
-        # Generate alignment for ecoevolity
-        gen_ecoevol_alignment(dir=rep_dir, config=config, gene_trees=gene_trees)
+        # Generate alignments with error
+        gen_error(dir=rep_dir, rng=rng, config=config)
+
+        for align_dir in glob.glob(os.path.join(rep_dir, "singleton-prob-*")): 
+            # Generate xml 
+            gen_xml(dir=align_dir, config=config)
+
+            # Generate alignment for ecoevolity
+            gen_ecoevol_alignment(dir=align_dir, config=config, gene_trees=gene_trees)
+
+
 
 if __name__ == "__main__":
     fire.Fire(gen_data)
