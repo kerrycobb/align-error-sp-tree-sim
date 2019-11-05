c5d8cddc19e1ae8d9f399d3f059862a8ef6f7b6b
Kerry Cobb <cobbkerry@gmail.com>
2019-11-05 17:51:59 -0600

    changes to run.py and get_snp_align to analyze snp only alignments

--- sims/get_snp_align.py
+++ sims/get_snp_align.py
@@ -48,19 +48,26 @@ def get_snps(matrix, single=True):
                 break 
     return snp_cols, polymorphic_sites
 
-def get_snp_align(dir, single=False):
+def get_snp_align(dir, single=False, overwrite=False):
     """
-    Creat alignment of SNPs only 
+    Creat alignment of SNPs only, use --single to only select a single snp 
     """
     config = yaml.safe_load(open(os.path.join(dir, "config.yml")))
     parent_dir = pathlib.Path(dir).parent
     if single:
-        new_dir = os.path.join(parent_dir, "single-snp")
+        new_dir = os.path.join(parent_dir, os.path.basename(dir) + "-snp")
     else:
-        new_dir = os.path.join(parent_dir, "all-snps")
+        new_dir = os.path.join(os.path.dirname(dir), os.path.basename(dir) + "-all-snps")
+    if overwrite:
+        try:
+            shutil.rmtree(new_dir) 
+        except:
+            pass
     os.mkdir(new_dir) 
     shutil.copy(os.path.join(dir, "config.yml"), new_dir) 
-    shutil.copy(os.path.join(dir, "eco-config.yml"), new_dir)
+    eco_config = yaml.safe_load(open(os.path.join(dir, "eco-config.yml")))
+    eco_config["global_comparison_settings"]["constant_sites_removed"] = True
+    yaml.dump(eco_config, open(os.path.join(new_dir, "eco-config.yml"), "w"))
     for rep in range(0, config["nreps"]):
         rep_dir = "rep-{}".format(rep)
         rep_dir_path = os.path.join(dir, rep_dir)
@@ -81,6 +88,7 @@ def get_snp_align(dir, single=False):
         new_align_path = os.path.join(new_rep_dir_path, "snp-alignment.phy")
         new_align = matrix_to_align(ids, concat_matrix)
         AlignIO.write(new_align, open(new_align_path, "w"), "phylip")
+    print("Finished getting SNP alignemnt from {}".format(dir))
 
 if __name__ == "__main__":
     fire.Fire(get_snp_align)
